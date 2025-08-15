00741eb7ad8910a8352828326913ca0a0b9e8df6
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-08-15 19:34:01 +0700

    remove constructor in StatsView

--- stats/stats-dto/src/main/java/ru/practicum/StatsView.java
+++ stats/stats-dto/src/main/java/ru/practicum/StatsView.java
@@ -1,17 +1,10 @@
 package ru.practicum;
 
-import lombok.*;
-
+import lombok.Data;
 
 @Data
 public class StatsView {
     private final String app;
     private final String uri;
     private final Integer hits;
-
-    public StatsView(String app, String uri, Integer hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
 }
