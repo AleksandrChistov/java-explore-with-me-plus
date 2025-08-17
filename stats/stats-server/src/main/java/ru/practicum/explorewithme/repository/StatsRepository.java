cf245b2227c7bb3cb7d488ca0e06eedfdcd48280
Kanava812 <knvznr@ya.ru>
2025-08-17 04:16:20 +0300

    добавлено: сохранение статистики в БД и её получение по запросу

--- stats/stats-server/src/main/java/ru/practicum/explorewithme/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/explorewithme/repository/StatsRepository.java
@@ -14,15 +14,13 @@ import java.util.List;
 @Repository
 public interface StatsRepository extends JpaRepository<Stats, Integer> {
 
-    @Query(value = " SELECT app, uri, COUNT(id) AS countHits FROM hits WHERE timestamp BETWEEN :start AND :end AND " +
-            "(:uris IS NULL OR uri IN :uris) GROUP BY app, uri ORDER BY countHits DESC ")
-    List<StatsView> findAllByTimestampBetweenAndUriIn(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("uris") List<String> uris
-    );
+    @Query(value = "SELECT app, uri, COUNT(id) AS countHits FROM stats WHERE timestamp BETWEEN :start AND :end " +
+            "AND (:uris IS EMPTY OR uri IN :uris) GROUP BY app, uri ORDER BY countHits DESC")
+    List<StatsView> findAllByTimestampBetweenAndUriIn(@Param("start") LocalDateTime start,
+                                                      @Param("end") LocalDateTime end,
+                                                      @Param("uris") List<String> uris);
 
-    @Query(value = " SELECT app, uri, COUNT(DISTINCT ip) AS uniqueIps FROM hits WHERE timestamp BETWEEN :start AND :end " +
+    @Query(value = " SELECT app, uri, COUNT(DISTINCT ip) AS uniqueIps FROM stats WHERE timestamp BETWEEN :start AND :end " +
             "AND (:uris IS NULL OR uri IN :uris) GROUP BY app, uri ORDER BY uniqueIps DESC ")
     List<StatsView> findAllUniqueIpAndTimestampBetweenAndUriIn(
             @Param("start") LocalDateTime start,
