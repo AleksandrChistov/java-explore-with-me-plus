f858b7aa0b5f66fc78b283bb2941c7eb38993ee9
Kanava812 <knvznr@ya.ru>
2025-09-04 18:53:38 +0300

    Feature. Работа с комментариями для зарегистрированных пользователей

--- main-service/src/main/java/ru/practicum/explorewithme/event/comment/controller/PrivateCommentController.java
+++ main-service/src/main/java/ru/practicum/explorewithme/event/comment/controller/PrivateCommentController.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.event.comment.dto.NewCommentDto;
 import ru.practicum.explorewithme.event.comment.dto.ResponseCommentDto;
@@ -12,7 +13,7 @@ import ru.practicum.explorewithme.event.comment.service.PrivateCommentService;
 
 @RestController
 @Slf4j
-@RequestMapping("/users/{userId}")
+@RequestMapping(path = "/users/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class PrivateCommentController {
 
@@ -27,6 +28,7 @@ public class PrivateCommentController {
         return service.create(userId, eventId, dto);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/comments/{commentId}")
     public void delete(@PathVariable @Positive Long userId,
                        @PathVariable @Positive Long commentId) {
@@ -34,6 +36,7 @@ public class PrivateCommentController {
         service.delete(userId, commentId);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/comments/{commentId}")
     public ResponseCommentDto patch(@PathVariable @Positive Long userId,
                                     @PathVariable @Positive Long commentId,
