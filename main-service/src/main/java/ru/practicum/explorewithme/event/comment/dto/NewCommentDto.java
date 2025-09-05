9d30563bcb201f09f79e44f81094e7571dd2d826
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-09-05 20:49:58 +0700

    remove redundant fields from NewCommentDto

--- main-service/src/main/java/ru/practicum/explorewithme/event/comment/dto/NewCommentDto.java
+++ main-service/src/main/java/ru/practicum/explorewithme/event/comment/dto/NewCommentDto.java
@@ -1,8 +1,6 @@
 package ru.practicum.explorewithme.event.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -14,10 +12,4 @@ import lombok.*;
 public class NewCommentDto {
     @NotBlank
     private String text;
-    @NotNull
-    @Positive
-    private Long eventId;
-    @NotNull
-    @Positive
-    private Long authorId;
 }
